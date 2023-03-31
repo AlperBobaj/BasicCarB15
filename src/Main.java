7db52059f910ca818ad1ce52e3fb7bdf5496f507
alper bobaj <alperbobaj@alpers-mbp-2.myfiosgateway.com>
2023-03-30 23:34:41 -0400

    has ben added

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
 
 
 
-        
+
         BasicStructure audi=new BasicStructure();
         audi.color="black";
         audi.model="A3";
         audi.power="5000cc";
+
+        System.out.println("selamun Aleykum");
     }
 }
